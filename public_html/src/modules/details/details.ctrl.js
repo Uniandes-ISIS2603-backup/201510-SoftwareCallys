59cbfc17e6b1ead99e2000435c37ea83cb745758
asandovaluniandes <a.sandoval1303@uniandes.edu.co>
2015-03-05 02:38:44 -0500

    Guardar datos de camiseta para carrito
    Ya están los métodos para actualizar solo me falta referenciar la lista
    del carrito de compras que no lo logré hacer

--- public_html/src/modules/details/details.ctrl.js
+++ public_html/src/modules/details/details.ctrl.js
@@ -1,3 +1,22 @@
+function agregarCompra(){
+    var nombre = "Camiseta 1";
+    var cantidad = 1;
+    var precio = "$300";
+    var talla = document.getElementById("Size:");
+    var color = document.getElementById("Color:");
+    
+    sessionStorage.setItem(nombre,cantidad,precio,talla,color);
+
+    display(nombre);
+}
+function display(nombre){
+  var lista = document.getElementById("sub-icon1 list");
+  var cantidad = sessionStorage.getItem(nombre);
+  var precio = sessionStorage.getItem(cantidad);
+  var talla = sessionStorage.getItem(precio);
+  var color = sessionStorage.getItem(talla);
+  lista.innerHTML = nombre + " Qty:" + cantidad + ". Price: " +precio + ". Size: " + talla + ". Color:" + color +"<br />";
+}
 (function () {
 	var app = angular.module('detailsModule');
 
