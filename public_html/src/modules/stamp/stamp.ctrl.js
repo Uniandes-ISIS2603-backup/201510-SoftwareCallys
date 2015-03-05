1a968d57b0b54c8cccde8e7d131e4613161a83b8
Samuel Baquero <sabame360@hotmail.com>
2015-03-04 23:23:19 -0500

    Añadida la opción de dar rate a las estampas.
    Aún no se muestran las estrellas.

--- public_html/src/modules/stamp/stamp.ctrl.js
+++ public_html/src/modules/stamp/stamp.ctrl.js
@@ -7,7 +7,34 @@
                          this.editMode=false;
             this.upload= function(){
                 this.editMode=true;
-            };
+            }; 
             }]);
-           
+        app.directive('stampsRating', function(){
+           return{
+               restrict: 'A',
+               template: '<ul class="rating">'+
+                           '<li ng-repeat="star in stars" ng-class="star" ng-click="toggle($index)">'+
+                             '\u2605' +
+                           '</li>' +
+                         '</ul>',
+               scope: {
+                   ratingValue: '=',
+                   max: '='
+               },
+               link: function (scope, elem, attrs){
+                   var updateStars = function (){
+                        scope.stars = [];
+                        for(var i = 0; i < scope.max; i++){
+                            scope.stars.push({filled: i < scope.ratingValue});
+                        }
+                   };
+                   scope.toggle = function(index){
+                       scope.ratingValue = index+1;
+                   };
+                   scope.$watch('ratingValue', function(oldVal, newVal) {
+                       if(newVal){updateStars();};
+                   });
+               }
+           } 
+        });   
 })();
