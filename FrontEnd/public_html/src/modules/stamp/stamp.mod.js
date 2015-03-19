8ed1e617de5e01723228e2dc7d1497410e99687b
syvelasquez10 <sy.velasquez10@uniandes.edu.co>
2015-03-19 02:02:59 -0500

    classpath

--- FrontEnd/public_html/src/modules/stamp/stamp.mod.js
+++ FrontEnd/public_html/src/modules/stamp/stamp.mod.js
@@ -1,9 +1,12 @@
 (function () {
-    var stampModule = angular.module('stampModule', ['CrudModule', 'MockModule','catalogModule']);
+    var stampModule = angular.module('stampModule', ['CrudModule', 'MockModule','ngResource','catalogModule']);
 
     stampModule.constant('stamp.context', 'stamps');
 
     stampModule.config(['stamp.context', 'MockModule.urlsProvider', function (context, urlsProvider) {
             urlsProvider.registerUrl(context);
         }]);
+    stampModule.factory('stampResource', function($resource){
+        return $resource('http://localhost:8080/resources/webservices/stamp', {},{ query: {method:'GET', params:{}, isArray:false} });
+    });
 })();
