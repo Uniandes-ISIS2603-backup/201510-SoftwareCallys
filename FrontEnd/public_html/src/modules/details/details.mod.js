9d24f09240a6861351cb1e926204e326f1e3e458
Luis Miguel Mejía <lm.mejia93@gmail.com>
2015-03-25 17:01:26 -0500

    cambios front end details module
    solo falta hacer editable la informacion de la camisa

--- FrontEnd/public_html/src/modules/details/details.mod.js
+++ FrontEnd/public_html/src/modules/details/details.mod.js
@@ -1,6 +1,6 @@
 (function ()
 {
-    var detailsModule = angular.module('detailsModule', ['CrudModule', 'MockModule','shirtModule']);
+    var detailsModule = angular.module('detailsModule', ['CrudModule', 'MockModule','shirtModule','catalogModule']);
     detailsModule.constant('details.context', 'details');
     detailsModule.config(['details.context', 'MockModule.urlsProvider', function (context, urlsProvider) 
     {
