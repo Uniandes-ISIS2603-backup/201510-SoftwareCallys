b6334da7219de03e65aca6b3720f7c2e141c8cce
asandovaluniandes <a.sandoval1303@uniandes.edu.co>
2015-04-28 10:46:34 -0500

    Selenium

---
+++ BackEnd/Callys.web.test/src/test/java/co/edu/uniandes/Callys/web/test/CallysTest.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.Callys.web.test;
+
+import static com.thoughtworks.selenium.SeleneseTestBase.fail;
+import java.util.concurrent.TimeUnit;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ *
+ * @author estudiante
+ */
+public class CallysTest {
+      // Es la instancia inicial del web driver que controlará el navegador firefox
+        private static WebDriver driver;
+        // url en el cual se aloja la página web (en este caso localhost:8080)
+        private static String baseUrl;
+        // variable que indica si varios alert consecutivos (alert javascript) se tomarán
+        private static boolean acceptNextAlert = true;
+        private static StringBuffer verificationErrors = new StringBuffer();
+        
+        /*La anotación @BeforeClass indica lo que se debe ejecutar ANTES de correr el archivo de pruebas. Este método instancia un nuevo driver firefox (causando la apertura de una ventana física de firefox).*/
+        @BeforeClass
+        public static void setUp() throws Exception {
+            driver = new FirefoxDriver();
+        // se define el url base del proyecto web
+            baseUrl = "http://localhost:8080";
+        /* Indica cuanto se espera para la respuesta de cualquier comando realizado hacia el navegador*/
+            driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        }
+        
+        // La anotación @AfterClass indica lo que se debe ejecutar DESPUES de ejecutar
+        // el archivo de pruebas. Este método cierra la ventana de firefox
+        // abierta por @BeforeClass que se utilizó para la prueba.
+        @AfterClass
+        public static void tearDown() throws Exception {
+            // Se cierra el navegador.
+            driver.quit();
+           // Se verifica que se haya cerrado efectivamente el navegador.
+            String verificationErrorString = verificationErrors.toString();
+            if (!"".equals(verificationErrorString)) {
+                fail(verificationErrorString);
+            }
+        }
+        
+}
