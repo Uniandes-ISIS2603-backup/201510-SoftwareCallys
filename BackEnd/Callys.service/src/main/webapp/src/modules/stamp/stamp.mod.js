5d2148ae51ae2754fb374bd076bef499b2686976
Sergio Velásquez <sy.velasquez10@uniandes.edu.co>
2015-04-28 22:12:53 -0500

    Conexión Backend-FrontEnd
    Cambios en el crud y en el modulo de estampa

--- BackEnd/Callys.service/src/main/webapp/src/modules/stamp/stamp.mod.js
+++ BackEnd/Callys.service/src/main/webapp/src/modules/stamp/stamp.mod.js
@@ -2,8 +2,9 @@
 {
     var stampModule = angular.module('stampModule', ['CrudModule', 'MockModule','catalogModule']);
     stampModule.constant('stamp.context', 'stamps');
-    stampModule.config(['stamp.context', 'MockModule.urlsProvider', function (context, urlsProvider)
+    stampModule.constant('stamp.skipMock', true);
+    stampModule.config(['stamp.context', 'MockModule.urlsProvider','stamp.skipMock', function (context, urlsProvider,skipMock)
     {
-        this.url = urlsProvider.registerUrl(context);
+        this.url = urlsProvider.registerUrl(context,skipMock);
     }]);
 })();
