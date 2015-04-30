602f39384467517a9b6301d25cdca95e210e9235
asandovaluniandes <a.sandoval1303@uniandes.edu.co>
2015-04-30 08:17:13 -0500

    Revert cambios conexion

---
+++ BackEnd/Callys.logic/src/main/java/co/edu/uniandes/Callys/carroCompras/logic/api/ICarroCompras.java
@@ -0,0 +1,22 @@
+package co.edu.uniandes.Callys.carroCompras.logic.api;
+
+import co.edu.uniandes.Callys.artista.logic.dto.ArtistaPageDTO;
+import co.edu.uniandes.Callys.carroCompras.logic.dto.CarroComprasDTO;
+import co.edu.uniandes.Callys.carroCompras.logic.dto.CarroComprasPageDTO;
+import java.util.List;
+
+public interface ICarroCompras {
+    public CarroComprasDTO createCarroCompras(CarroComprasDTO detail);
+
+    public List<CarroComprasDTO> getCarrosCompras();
+
+    public CarroComprasPageDTO getCarrosCompras(Integer page, Integer maxRecords);
+
+    public CarroComprasDTO getCarroCompras(Long id);
+
+    public void deleteCarroCompras(Long id);
+
+    public void updateCarroCompras(CarroComprasDTO detail);
+
+    public ArtistaPageDTO getArtistas(Integer page, Integer maxRecords);
+}
