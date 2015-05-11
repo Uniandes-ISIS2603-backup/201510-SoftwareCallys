526111e105b5f65b81c33acbef65d9dbe0a9925c
asandovaluniandes <a.sandoval1303@uniandes.edu.co>
2015-05-11 11:38:09 -0500

    Correccion login

--- BackEnd/Callys.service/src/main/java/co/edu/uniandes/Callys/services/ClienteService.java
+++ BackEnd/Callys.service/src/main/java/co/edu/uniandes/Callys/services/ClienteService.java
@@ -60,10 +60,10 @@ public class ClienteService {
     public void updateCliente(ClienteDTO cliente) {
         clienteLogicService.updateCliente(cliente);
     }
-    /*
+    
     @GET
     public Integer loginCliente(@QueryParam("name") String name, @QueryParam("password") String password){
         return clienteLogicService.loginCliente(name, password);
     }
-    */
+    
 }
