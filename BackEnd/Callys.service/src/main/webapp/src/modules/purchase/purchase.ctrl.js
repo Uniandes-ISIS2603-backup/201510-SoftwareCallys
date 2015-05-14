a2bac637e8b3f729874e033957f1f57713b547e4
Sergio Velásquez <sy.velasquez10@uniandes.edu.co>
2015-05-13 23:34:08 -0500

    arreglo en purchase

--- BackEnd/Callys.service/src/main/webapp/src/modules/purchase/purchase.ctrl.js
+++ BackEnd/Callys.service/src/main/webapp/src/modules/purchase/purchase.ctrl.js
@@ -4,13 +4,12 @@
     purchaseModule.controller('purchaseCtrl', ['$scope', 'purchaseService', function ($scope, purchaseService)
     {
         purchaseService.extendCtrl(this, $scope);
-        this.viewPurchases(1);
         
-        this.cliente=1;
-
+        this.noRate=false;
         this.viewPurchases= function(cliente)
         {
             this.currentRecord=purchaseService.getPurchasesByClient(cliente);
         };
+        this.viewPurchases(2);
     }]);
 })();
